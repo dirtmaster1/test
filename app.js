0c6f16d792ee30187cc5c32200cb858875b5d9c9
Anthony Frazier <anthonyfrazier@hotmail.com>
2015-08-31 21:44:39 -0500

    set up app.js to load index.ejs on load

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 var express = require('express');
 var app = express();
-module.exports = app;
+
+var port = process.env.Port || 3000;
+
+app.get('/', function(req, res){
+    res.render('index.ejs');
+});
+
+app.listen(port, function(){
+    console.log('Server running at port: ' + port);
+});
+
+
