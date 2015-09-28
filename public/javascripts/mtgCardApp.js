85eb99dbda5f43f1560e3ddf06b0d84f9e3b330b
Anthony Frazier <anthonyfrazier@hotmail.com>
2015-09-28 13:23:53 -0500

    set up angular controller

---
+++ public/javascripts/mtgCardApp.js
@@ -0,0 +1,24 @@
+(function(){
+
+    var app = angular.module('mtgCardApp',[]);
+
+    var CardCtrl = function($scope, $http){
+        $scope.message = 'it works...';
+
+        var onComplete = function(response){
+            $scope.data = response.data;
+        }
+
+        var onError = function(err){
+            $scope.error = err;
+        }
+
+        $http.get("http://localhost:8000/api/cards")
+            .then(onComplete, onError);
+
+
+    };
+
+    app.controller('CardCtrl',["$scope", "$http", CardCtrl]);
+})()
+
