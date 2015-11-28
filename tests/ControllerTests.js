2f5dc93c6d0f63c36f23277b412effd1d2a159b7
Anthony Frazier <anthonyfrazier@hotmail.com>
2015-11-27 22:59:44 -0600

    set up for testing

---
+++ tests/ControllerTests.js
@@ -0,0 +1,10 @@
+var should = require('should'),
+    sinon = require('sinon');
+
+describe('Controller tests', function(){
+    describe('UpdateGrid tests', function(){
+        it('should update selected cards properly', function(){
+            (1+1).should.equal(3);
+        })
+    })
+})
