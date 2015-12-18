1fb4af27a567a5d87a1da9bab0c396a66b480083
Anthony Frazier <anthonyfrazier@hotmail.com>
2015-12-17 21:45:37 -0600

    set up karma

--- tests/ControllerTests.js
+++ tests/ControllerTests.js
@@ -1,10 +1,7 @@
-var should = require('should'),
-    sinon = require('sinon');
-
 describe('Controller tests', function(){
     describe('UpdateGrid tests', function(){
         it('should update selected cards properly', function(){
-            (1+1).should.equal(3);
+            expect(1+1).toBe(2);
         })
     })
 })
