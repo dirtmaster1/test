9cbc3f4e2fd95d7f7a61bbf1af6a334bc8577d85
Anthony Frazier <anthonyfrazier@hotmail.com>
2018-09-21 00:10:28 -0500

    added tunnel for collision testing

--- public/javascripts/Game/MapBuilder.js
+++ public/javascripts/Game/MapBuilder.js
@@ -7,46 +7,85 @@ game.mapBuilder = (function ()
 	
 	function Init(scene, gameObjects)
 	{
-		 var x = 0;
-		 var y = 0;
-		 var z = 0;
-		 
-		 // section 1
-		 var tileCube1 = graphics.CreateBox(0xeb2300, 100, 100, 10);
-		 tileCube1.position.set(x, y, z - 5);
-		 
-		 var tileCube2 = graphics.CreateBox(0xeb2300, 100, 100, 10);
-		 tileCube2.position.set(x, y, z + 105);
-		 
-		 var tileCube3 = graphics.CreateBox(0xeb2300, 10, 100, 100);
-		 tileCube3.position.set(x + 55, y, z + 50);
-		 
-		 var tileCube4 = graphics.CreateBox(0xeb2300, 10, 100, 100);
-		 tileCube4.position.set(x - 55, y, z + 50);
-		 
-		 // section 1
-		 var tileCube5 = graphics.CreateBox(0xeb2300, 100, 100, 10);
-		 tileCube5.position.set(x, y + 100, z - 5);
-		 
-		 var tileCube6 = graphics.CreateBox(0xeb2300, 100, 100, 10);
-		 tileCube6.position.set(x, y + 100, z + 105);
-		 
-		 var tileCube7 = graphics.CreateBox(0xeb2300, 10, 100, 100);
-		 tileCube7.position.set(x + 55, y + 100, z + 50);
-		 
-		 var tileCube8 = graphics.CreateBox(0xeb2300, 10, 100, 100);
-		 tileCube8.position.set(x - 55, y + 100, z + 50);
-		 
-		 scene.add(tileCube1);
-		 scene.add(tileCube2);
-		 scene.add(tileCube3);
-		 scene.add(tileCube4);
-		 
-		 scene.add(tileCube5);
-		 scene.add(tileCube6);
-		 scene.add(tileCube7);
-		 scene.add(tileCube8);
+		var count = 100;
+		var startPosition = { x:0, y:0, z:0 }
+		var shaftLength = 100;
+		var shaftWallSize = 100;
+		var shaftDirectionAxis = 'z';
+		
+		for (var i =0; i < count; i++)
+		{
+			CreateOpenShaft(scene, 
+							startPosition, 
+							shaftWallSize, 
+							shaftLength,
+							shaftDirectionAxis); 
+							
+			startPosition.z -= shaftLength;
+		}
     }
+	
+	function CreateOpenShaft(scene, pos, size, length, direction)
+	{
+		 var thickness = 1;
+		 
+		 if(!size)
+		 {
+			size = 100;
+		 }
+		 
+		 if(!length)
+		 {
+			 length = 100;
+		 }
+		 
+		 if(direction == 'z')
+		 {
+			var tileCube1 = graphics.CreateBox(0xfdfe02, size, thickness, size);
+			tileCube1.position.set(pos.x, pos.y + length/2, pos.z + length/2);
+			
+			//green
+			var tileCube2 = graphics.CreateBox(0x0bff01, size, thickness, size);
+			tileCube2.position.set(pos.x, pos.y - length/2, pos.z + length/2);
+			
+			//blue
+			var tileCube3 = graphics.CreateBox(0x011efe, thickness, size, size);
+			tileCube3.position.set(pos.x + length/2, pos.y, pos.z + length/2);
+			
+			//purple
+			var tileCube4 = graphics.CreateBox(0xfe00f6, thickness, size, size);
+			tileCube4.position.set(pos.x - length/2, pos.y, pos.z + length/2);
+			
+			scene.add(tileCube1);
+			scene.add(tileCube2);
+			scene.add(tileCube3);
+			scene.add(tileCube4); 
+		 }
+		 
+		 if(direction == 'y')
+		 {
+			 //yellow
+			 var tileCube1 = graphics.CreateBox(0xfdfe02, size, size, thickness);
+			 tileCube1.position.set(pos.x, pos.y, pos.z);
+			 
+			 //green
+			 var tileCube2 = graphics.CreateBox(0x0bff01, size, size, thickness);
+			 tileCube2.position.set(pos.x, pos.y, pos.z + length);
+			 
+			 //blue
+			 var tileCube3 = graphics.CreateBox(0x011efe, thickness, size, size);
+			 tileCube3.position.set(pos.x + length/2, pos.y, pos.z + length/2);
+			 
+			 //purple
+			 var tileCube4 = graphics.CreateBox(0xfe00f6, thickness, size, size);
+			 tileCube4.position.set(pos.x - length/2, pos.y, pos.z + length/2);
+			 
+			 scene.add(tileCube1);
+			 scene.add(tileCube2);
+			 scene.add(tileCube3);
+			 scene.add(tileCube4);
+		 }
+	}
 
     return{
         Init: Init
