d6b3f41be043e5812a1f341d2ffce98f235dd812
Anthony Frazier <anthonyfrazier@hotmail.com>
2018-11-23 21:53:21 -0600

    added checkerboard grid

--- public/javascripts/tbs/tbs.js
+++ public/javascripts/tbs/tbs.js
@@ -20,7 +20,7 @@ tbs.run = (function() {
         document.getElementById("container").appendChild(renderer.domElement);
 		
 		scene.add(camera);
-		mapMandelbrotSet();
+		createGrid(scene);
     }
 
     function render() {
@@ -29,53 +29,55 @@ tbs.run = (function() {
         requestAnimationFrame(render);
     }
 	
-	function mapMandelbrotSet()
+	function createGrid(scene)
 	{
-		var iteration = 0;
-		var maxIterations = 10;
-		var z = [0,0];
-		var c = [0,0];
-		var c_re = 0;
-		var c_im = 0;
+		var grid = [
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0]
+		];
 		
-		var height = 10;
-		var width = 10;
+		var maxColumns = 10;
+		var maxRows = 10;
 		
-		for (var x=0; x < width; x++)
+		var tileWidth = 100;
+		var tileHeight = 100;
+		
+		var grid_origin_x = -400;
+		var grid_origin_y = -400;
+		
+		for(var row = 0; row < maxRows; row++)
 		{
-			for (var y=0; y < height; y++)
+			for(var column = 0; column < maxColumns; column++)
 			{
-				c_re = (x - width/2)*4.0/width;
-				c_im = (y - height/2)*4.0/width;
-				
-				
-				z = [0,0];
-				c = [c_re,c_im];
-				iteration = 0;
-				
-				while(iteration < maxIterations && (Math.pow(z[0],2) + Math.pow(z[1],2) <= 4))
+				var color = 0xff0000;
+			
+				if((row + column)%2 == 0)
 				{
-					z[0] = Math.pow(z[0],2) - Math.pow(z[1],2) + c[0];
-					z[1] = (2 * z[0] * z[1]) + c[1];
-					iteration++;										
+					color = 0x00ff00
 				}
 				
-				if(iteration < maxIterations)
-				{
-					var circle = drawCircle(x,y,-20);
-				    scene.add( circle );
-				}
+				var tile = createTile(color, tileWidth, tileHeight);
+				grid[row][column] = tile;
+				
+				tile.position.set(grid_origin_x + (column * tileWidth),
+								  grid_origin_y + (row * tileHeight),
+									-10);
+				scene.add(tile)	
 			}
 		}
 	}
 	
-	function drawCircle(x,y,z)
+	function createTile(color, width, height)
 	{
-		var geometry = new THREE.CircleGeometry( 1, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-		var circle = new THREE.Mesh( geometry, material );
-		circle.position.set(x,y,z);
-		
-		return circle;
+		return graphics.CreateBox(color, width, height, 1);
 	}
+	
 })();
